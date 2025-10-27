c9950441875e57787d3563d61a27a7f6528eab5e
Arta <artaroohani2009@gmail.com>
2025-10-27 06:28:59 +0200

    Add files via upload

---
+++ 1 - Variables in C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1___Variables_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabler
+            string name = "Arta";
+            int myNum1 = 15;
+            int myNum2 = 25;
+
+            // Print
+            Console.WriteLine(name);
+            Console.WriteLine(myNum1);
+            Console.WriteLine(myNum2);
+        }
+    }
+}
