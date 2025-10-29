c8c0ac5f7038186a047f5fa286e6825a45c267ec
Arta <artaroohani2009@gmail.com>
2025-10-29 09:18:48 +0200

    Add files via upload

---
+++ 08.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 10;
+
+            if (age >= 0)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
+            //CONDITION ? true : FALSE
+            string result = age >= 0 ? "Valid" : "Invalid";
+            Console.WriteLine(result);
+        }
+    }
+}
