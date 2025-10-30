b04c1f6798292f99bb9c5c0a7afae3e133e13655
Arta <artaroohani2009@gmail.com>
2025-10-30 11:59:06 +0200

    Add files via upload

---
+++ 4.5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age: ");
+            string ageInput = Console.ReadLine();
+            int age = Convert.ToInt32(ageInput);
+            if (age >= 18)
+            {
+                Console.WriteLine("Your 18+ :D");
+            }
+            else
+            {
+                Console.WriteLine("Not old enough");
+            }
+            //Boo!did i scare you?
+        }
+    }
+}
