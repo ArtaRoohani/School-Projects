3ae063456f96a02503340d02859c2320192cc6d3
Arta <artaroohani2009@gmail.com>
2025-10-30 13:10:11 +0200

    Add files via upload

---
+++ 07.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setting the string counter to 1
+            int Counter = 1;
+            int addBy = 1;
+
+            //While loop part
+            while (Counter <= 5)
+            {   Console.WriteLine("While Loop Counter: " + Counter);
+                Counter += addBy;
+            }
+        }
+    }
+}
