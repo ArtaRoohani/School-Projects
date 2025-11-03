8f6206ab7428ab372b07cbb300f6a23f3d3dd156
Arta <artaroohani2009@gmail.com>
2025-11-03 12:43:15 +0200

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StringLooping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the message: C# is fun I am going to code C# thomorrow ");
+            var message = Console.ReadLine();
+
+            char[] messageArray = message.ToCharArray();
+            Array.Reverse(messageArray);
+
+            //print massage in reverse order from top to bottom
+            foreach (char c in messageArray)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(100); // Pause for 100 milliseconds
+            }
+
+            //Making space between the two outputs in a really tupid way
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            //print massage normally from top to bottom
+            //I dont like pinnaple on pizza by the way
+
+
+            foreach (char c in message)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(100); // Pause for 100 milliseconds
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                       //Its here for no reason at all but dont worry, it wont affect the code (hopefully)
+
+                 //          ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⡀⠀⠀⠀⠀⢀⣀⣀⡀⠀⠀
+                ⠀//⠀⠀⠀ ⠀⣀⣀⡀⠀⠀⠀⠀⢀⣀⣀⣀⡀⠘⠋⢉⠙⣷⠀⠀⢀⣾⣿⡿⠁⠀⠀
+                 //⠀⠀⢀⣴⣿⡿⠋⣉⠁⣠⣾⣿⣿⣿⣿⡿⠿⣦⡈⠀⣿⡇⠀⣼⣿⣿⠃⠀⠀⠀
+                 //⠀⠀⠀⣽⣿⣧⠀⠃⢰⣿⣿⡏⠙⣿⠿⢧⣀⣼⣷⠀⡿⠃⣰⣿⣿⡏⠀⠀⠀⠀
+                 //⠀⠀⠀⠉⣿⣿⣦⠀⢿⣿⣿⣷⣾⡏⠀⠀⢹⣿⣿⠀⠀⢰⣿⣿⡟⠀⠀⠀⠀⠀
+                 //⠀⠀⠀⠀⠀⠉⠛⠁⠈⠿⣿⣿⣿⣷⣄⣠⡼⠟⠁⠀⢠⣿⣿⡿⠁⠀⠀⠀⠀⠀
+                ⠀//⠀⠀⠀⠀ ⠀⠀⠀⢀⣤⣤⣄⣉⣉⣉⣠⡀⠀⠀⢀⡿⠿⢿⠃⠀⠀⠀⠀⠀⠀
+                ⠀//⠀⠀   ⠀⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣤⣤⣶⣄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀              ⠀//⠀⠀   ⢀⣾⣿⣿⣿⣿⠟⠋⣉⣉⠙⠻⣿⣿⣿⣿⠿⠋⠀⠀⠀⠀⠀⠀⠀
+⠀              ⠀⠀//⠀  ⠀⣾⣿⣿⣿⡏⢀⣴⣿⣿⣿⣿⡄⢀⣤⣤⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀               ⠀//⠀  ⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢀⣤⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀// ⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣠⣿⣿⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               ⠀⠀//⠀ ⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿⣯⣠⣾⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀                //⠀ ⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⣿⠿⠋⣠⡾⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                 //⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀
