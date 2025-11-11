279f2bc3882715ae42868d972379ae2be0ac7884
Arta <artaroohani2009@gmail.com>
2025-11-11 13:35:46 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,29 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace InputOutput
+namespace logicError2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Output & Input
-            //Console.WriteLine("This is a Test");
 
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();  
-            //Console.WriteLine(name);  
+            int age = 19;
 
-            Console.Write("Enter your age: ");
-            string ageInput = Console.ReadLine();
-            int age = Convert.ToInt32(ageInput);
-
-            //Console.Write("Your name is ");
-            //Console.WriteLine(name);
-            //Console.Write(" and your age is ");
-            //Console.WriteLine(age);
-
-            Console.WriteLine("Your name is "+ name +" and your age is " + age);
+            if (age > 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+            else if (age > 30)
+            {
+                Console.WriteLine("Over 30");
+            }
+            else if (age > 18)
+            {
+                Console.WriteLine("Over 18");
+            }
             Console.ReadLine();
         }
     }
