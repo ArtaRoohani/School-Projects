cd8d14b2b7bed5eb38173a38b3a1a5465e0c1212
Arta <artaroohani2009@gmail.com>
2025-11-14 12:46:27 +0200

    Add files via upload

---
+++ 20.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20
+{
+    class Program
+    {
+        class Circle
+        {
+            void Main(string[] args)
+            {
+                double Radius = 1.5;
+                double area = CalculateArea(Radius);
+                Console.WriteLine($"Math PI ({Math.PI}) * Radius ({Radius}) * Radius ({Radius}) = {area}");
+                Console.WriteLine($"Area of the chircle is: {area.ToString(CultureInfo.InvariantCulture)}");
+            }
+
+            private static double CalculateArea(double Radius)
+            {
+                return Math.PI * Radius * Radius;
+            }
+        }
+    }
+}
