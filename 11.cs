9adb6b2a99ce8b129501e631bbd26c4c900c075d
Arta <artaroohani2009@gmail.com>
2025-11-14 09:41:46 +0200

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StringLooping
+{
+    internal class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string message = "Hello, World!";
+            string reversedText = ReverseString(message);
+
+            Console.WriteLine("Original message:");
+            foreach (char c in message)
+            {
+                Console.WriteLine(c); 
+            }
+
+            Console.WriteLine("\nReversed message:"); 
+            foreach (char c in reversedText)
+            {
+                Console.WriteLine(c);
+            }
+        }
+
+        public static string ReverseString(string text)
+        {
+            char[] charArray = text.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
+
+//Hejsan :) 
