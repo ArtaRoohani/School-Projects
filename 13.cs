79c1a26a1980d726e30ee0d95d5b6eb5f8222026
Arta <artaroohani2009@gmail.com>
2025-11-25 10:45:51 +0200

    Add files via upload

---
+++ 13.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt for the player to enter a number
+            Console.WriteLine("Enter a number:");
+            int number = int.Parse(Console.ReadLine());
+            //Checking if the number is even or odd
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
+        }
+    }
+}
